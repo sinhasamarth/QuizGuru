5378deecbcbef033887b3cd7518019310b059399
Samarth Sinha <samarthkumar192@gmail.com>
2022-01-23 18:37:31 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/github/sinhasamarth/quizguru/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.sinhasamarth.quizguru.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.sinhasamarth.quizguru.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
